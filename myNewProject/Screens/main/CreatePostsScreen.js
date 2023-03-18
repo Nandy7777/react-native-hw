d39d39395696a7e6de6afc10095df0c52869a57c
Nadiia Kotyk <99249549+Nandy7777@users.noreply.github.com>
2023-03-18 19:24:21 +0200

    camera works

--- myNewProject/Screens/main/CreatePostsScreen.js
+++ myNewProject/Screens/main/CreatePostsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   View,
   Text,
@@ -8,31 +8,63 @@ import {
 } from "react-native";
 import { Camera } from "expo-camera";
 
+// import { StatusBar } from "expo-status-bar";
+// import { shareAsync } from "expo-sharing";
+import * as MediaLibrary from "expo-media-library";
+
 const CreatePostsScreen = () => {
+  // let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  
   const [camera, setCamera] = useState(null);
-  const [photo, setPhoto] = useState('');
+  const [photo, setPhoto] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+    })();
+  }, []);
+
+  if (hasCameraPermission === undefined) {
+     return <Text>Requesting permissions...</Text>;
+  } else if (!hasCameraPermission) {
+    return (
+      <Text>
+        Permission for camera not granted. Please change this in settings.
+      </Text>
+    );
+  }
 
   const takePhoto = async () => {
-    const photo = await camera.takePictureAsync();
-    setPhoto(photo.uri);
-    console.log('photo', photo);
+    const options = {
+      quality: 1,
+      base64: true,
+      exif: false,
+    };
+    const newPhoto = await camera.takePictureAsync(options);
+    setPhoto(newPhoto.uri);
   }
- 
+   
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} ref={setCamera}>
-        {photo && (
-          <View style={styles.takePhotoContainer}>
-            <Image
-              source={{ uri: photo }}
-              style={{ height: 200, width: 200 }}
-            />
-          </View>
-        )}
+        <View style={styles.takePhotoContainer}>
+          <Image source={{ uri: photo }} style={{ height: 200, width: 200 }} />
+        </View>
         <TouchableOpacity onPress={takePhoto} style={styles.snapContainer}>
           <Text style={styles.snap}>SNAP</Text>
         </TouchableOpacity>
       </Camera>
+      <View>
+        <TouchableOpacity onPress={takePhoto} style={styles.sendBtn}>
+          <Text style={styles.sendLabel}>SNAP</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -42,6 +74,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   camera: {
+    marginHorizontal: 16,
+    borderRadius: 8,
     height: 240,
     marginTop: 32,
     justifyContent: "center",
@@ -62,13 +96,27 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   takePhotoContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
-    borderColor: '#fff',
+    borderColor: "#fff",
     borderWidth: 1,
-    
-  }
+    borderRadius: 8,
+  },
+  sendBtn: {
+    marginHorizontal: 16,
+    marginTop: 207,
+    borderRadius: 100,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    height: 51,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sendLabel: {
+    color: "#BDBDBD",
+    fontSize: 16, 
+  },
 });
 
 export default CreatePostsScreen;
